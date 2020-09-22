c6f3c6e101a674044bee472cee72631ea2bedf0c
Anthler <bullanthla@gmail.com>
2020-09-22 00:17:30 +0000

    removed try catch block

--- index.ts
+++ index.ts
@@ -1,18 +1,12 @@
 import Jimp from "jimp/es";
 
-async function resize_and_watermark(url: string){
-    try {
+export async function resize_and_watermark(url: string){
         const font = await Jimp.loadFont(Jimp.FONT_SANS_16_WHITE);
         const image = await Jimp.read(url);
         image.resize(Jimp.AUTO, 250)
         image.quality(1)
         image.print(font, Jimp.HORIZONTAL_ALIGN_LEFT, Jimp.VERTICAL_ALIGN_TOP, "JHELY BOLIVIA")
         image.write('jimp-imgs/bg1-new.jpeg');
-    } catch (error) {
-        console.log(error.message);
-    }finally{
-        process.exit(0);
-    }
 }
 
 resize_and_watermark('./imgs/bg1.jpg');
